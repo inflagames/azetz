f7c79320a5d20d2d918b6d6f4255f8b3b916a0d9
Guillermo Gonzalez <guillermo.gonzalez.ext@syrocon.de>
2021-08-21 00:54:07 +0200

    Integrate button in menu scene

--- src/game/scenes/menu.js
+++ src/game/scenes/menu.js
@@ -1,6 +1,12 @@
 import Scene from "./scene";
+import Button from "../components/button";
 
 export default class Menu extends Scene {
+  constructor() {
+    super();
+    this.button = new Button(5, 7, 100, 30, "BUTTON");
+  }
+
   /**
    * @param context {CanvasRenderingContext2D}
    */
@@ -8,5 +14,6 @@ export default class Menu extends Scene {
     super.render(context);
 
     // toDo guille 20.08.21: render menu here
+    this.button.render(context);
   }
 }
