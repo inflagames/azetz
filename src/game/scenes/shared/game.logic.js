246c08d938965dc4670708e64a4aea0fe2d64828
Guillermo <ggjnez92@gmail.com>
2021-08-26 10:32:25 +0200

    Add acceleration/deceleration to the ship and integrate in the map

--- src/game/scenes/shared/game.logic.js
+++ src/game/scenes/shared/game.logic.js
@@ -1,9 +1,24 @@
 import {rotateVector} from "../../utils/helpers";
+import {FPS} from "../../game";
+
+const SHIP_ACCELERATING = "0";
+const SHIP_DECELERATION = "1";
+const SHIP_STOP = "3"
+
+const MAX_VELOCITY = 10;
 
 export default class GameLogic {
   constructor() {
     this.time = 0;
-    this.ship = {x: 200, y: 0, rotation: Math.PI / 2, velocity: 0};
+    this.ship = {
+      x: 200,
+      y: 0,
+      rotation: Math.PI / 2,
+      velocity: 0,
+      acceleration: 5,
+      deceleration: -2,
+      status: SHIP_STOP
+    };
   }
 
   play() {
@@ -15,14 +30,16 @@ export default class GameLogic {
    * @param rotation {number}
    */
   launchShip(rotation) {
+    this.time = 0;
     this.ship.rotation = rotation;
-    this.ship.velocity = 10;
+    this.ship.status = SHIP_ACCELERATING;
   }
 
   moveShip() {
     if (this.isFighting()) {
       this.wallCollision();
 
+      this.calculateVelocity();
       const distance = this.ship.velocity;
       const movement = rotateVector({x: distance, y: 0}, this.ship.rotation);
 
@@ -31,15 +48,30 @@ export default class GameLogic {
     }
   }
 
+  calculateVelocity() {
+    let velocity;
+    if (this.ship.status === SHIP_ACCELERATING) {
+      velocity = this.ship.velocity + this.ship.acceleration / FPS;
+      if (velocity > MAX_VELOCITY) {
+        this.ship.status = SHIP_DECELERATION;
+      }
+    }
+    if (this.ship.status === SHIP_DECELERATION) {
+      velocity = this.ship.velocity + this.ship.deceleration / FPS;
+      if (velocity <= 0) {
+        this.ship.status = SHIP_STOP;
+        velocity = 0;
+      }
+    }
+    this.ship.velocity = velocity;
+  }
+
   isFighting() {
-    return this.ship.velocity > 0;
+    return this.ship.status !== SHIP_STOP;
   }
 
   wallCollision() {
-    if (this.ship.rotation > Math.PI / 2 && this.ship.x < 10) {
-      this.ship.rotation = Math.PI - this.ship.rotation;
-    }
-    if (this.ship.rotation < Math.PI && this.ship.x > 390) {
+    if ((this.ship.rotation > Math.PI / 2 && this.ship.x < 10) || (this.ship.rotation < Math.PI && this.ship.x > 390)) {
       this.ship.rotation = Math.PI - this.ship.rotation;
     }
   }
