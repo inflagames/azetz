d3f0c95606ef850ebf3fb06aa109d02810550564
Guillermo <ggjnez92@gmail.com>
2021-08-26 15:14:39 +0200

    Add smooth rotation to the ship

--- src/game/scenes/shared/game.logic.js
+++ src/game/scenes/shared/game.logic.js
@@ -2,11 +2,13 @@ import {rotateVector} from "../../utils/helpers";
 import {FPS, SCREEN_WIDTH} from "../../game";
 
 const SHIP_ACCELERATING = "0";
-const SHIP_DECELERATION = "1";
-const SHIP_STOP = "3"
+const SHIP_DECELERATING = "1";
+const SHIP_STOP = "3";
+const SHIP_ROTATING = "4";
 
 const MAX_VELOCITY = 10;
-const MARGIN_TO_COLLIDE = 10;
+const MARGIN_TO_COLLIDE = 50;
+const TIME_TO_ROTATE_SHIP_MS = 400;
 
 export default class GameLogic {
   constructor() {
@@ -15,10 +17,11 @@ export default class GameLogic {
       x: SCREEN_WIDTH / 2,
       y: 0,
       rotation: Math.PI / 2,
+      expectedRotation: 0,
       velocity: 0,
       acceleration: 5,
       deceleration: -2,
-      status: SHIP_STOP
+      status: [SHIP_STOP]
     };
   }
 
@@ -33,7 +36,7 @@ export default class GameLogic {
   launchShip(rotation) {
     this.time = 0;
     this.ship.rotation = rotation;
-    this.ship.status = SHIP_ACCELERATING;
+    this.ship.status = [SHIP_ACCELERATING];
   }
 
   moveShip() {
@@ -41,6 +44,7 @@ export default class GameLogic {
       this.wallCollision();
 
       this.calculateVelocity();
+      this.rotateShip();
       const distance = this.ship.velocity;
       const movement = rotateVector({x: distance, y: 0}, this.ship.rotation);
 
@@ -49,18 +53,36 @@ export default class GameLogic {
     }
   }
 
-  calculateVelocity() {
-    let velocity;
-    if (this.ship.status === SHIP_ACCELERATING) {
-      velocity = this.ship.velocity + this.ship.acceleration / FPS;
-      if (velocity > MAX_VELOCITY) {
-        this.ship.status = SHIP_DECELERATION;
+  rotateShip() {
+    if (this.shipStatus() === SHIP_ROTATING) {
+      const rotationFactor = 1000 / FPS * Math.PI / TIME_TO_ROTATE_SHIP_MS;
+      if (this.ship.rotation > this.ship.expectedRotation) {
+        this.ship.rotation = Math.max(this.ship.rotation - rotationFactor, this.ship.expectedRotation);
+      } else {
+        this.ship.rotation = Math.min(this.ship.rotation + rotationFactor, this.ship.expectedRotation);
+      }
+      // stop rotation
+      if (this.ship.rotation === this.ship.expectedRotation) {
+        this.ship.status.pop();
       }
     }
-    if (this.ship.status === SHIP_DECELERATION) {
+  }
+
+  calculateVelocity() {
+    if (this.shipStatus() === SHIP_ROTATING) {
+      return;
+    }
+    let velocity = 0;
+    if (this.shipStatus() === SHIP_ACCELERATING) {
+      velocity = this.ship.velocity + this.ship.acceleration / FPS;
+      if (velocity > MAX_VELOCITY) {
+        this.ship.status = [SHIP_DECELERATING];
+      }
+    }
+    if (this.shipStatus() === SHIP_DECELERATING) {
       velocity = this.ship.velocity + this.ship.deceleration / FPS;
       if (velocity <= 0) {
-        this.ship.status = SHIP_STOP;
+        this.ship.status = [SHIP_STOP];
         velocity = 0;
       }
     }
@@ -68,13 +90,23 @@ export default class GameLogic {
   }
 
   isFighting() {
-    return this.ship.status !== SHIP_STOP;
+    return this.shipStatus() !== SHIP_STOP;
   }
 
   wallCollision() {
-    if ((this.ship.rotation > Math.PI / 2 && this.ship.x < MARGIN_TO_COLLIDE) ||
-      (this.ship.rotation < Math.PI && this.ship.x > SCREEN_WIDTH - MARGIN_TO_COLLIDE)) {
-      this.ship.rotation = Math.PI - this.ship.rotation;
+    if (this.shipStatus() !== SHIP_ROTATING &&
+      this.ship.rotation > Math.PI / 2 && this.ship.x < MARGIN_TO_COLLIDE) {
+      this.ship.status.push(SHIP_ROTATING);
+      this.ship.expectedRotation = Math.PI - this.ship.rotation;
     }
+    if (this.shipStatus() !== SHIP_ROTATING &&
+      this.ship.rotation < Math.PI / 2 && this.ship.x > SCREEN_WIDTH - MARGIN_TO_COLLIDE) {
+      this.ship.status.push(SHIP_ROTATING);
+      this.ship.expectedRotation = Math.PI - this.ship.rotation;
+    }
+  }
+
+  shipStatus() {
+    return this.ship.status[this.ship.status.length - 1];
   }
 }
